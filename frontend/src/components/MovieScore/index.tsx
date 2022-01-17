e8afc24a28f811a4ed70978f582fcfdd6718cd9b
Rafael C. Viana <rchagas.sdi@gmail.com>
2022-01-16 23:05:25 -0300

    correção-estrela-score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -15,7 +15,7 @@ function MovieScore({score, count }: Props) {
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars score={0} />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
